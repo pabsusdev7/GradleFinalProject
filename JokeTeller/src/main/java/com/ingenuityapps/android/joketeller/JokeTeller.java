fb4f29f7b6fabc62093db94d3a87f5a061900cf6
pabsusdev7 <pabsusdrumer@gmail.com>
2018-10-07 23:05:01 -0500

    Created JokeTeller java library and used by app to display joke.

---
+++ JokeTeller/src/main/java/com/ingenuityapps/android/joketeller/JokeTeller.java
@@ -0,0 +1,9 @@
+package com.ingenuityapps.android.joketeller;
+
+public class JokeTeller {
+
+    public String getJoke(){
+        return "Android: where ProgressBars go around in circles and Spinners don’t spin!";
+    }
+
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.ingenuityapps.android.joketeller.JokeTeller;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeTeller jokeTeller = new JokeTeller();
+        Toast.makeText(this, jokeTeller.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
